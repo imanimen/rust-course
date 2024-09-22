787a6a4b7ac5f2861abdf754ab6de600d6c2796b
iman <imanimen81@gmail.com>
2024-09-23 00:08:09 +0330

    chore(project structure)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![allow(unused)] // for ignoring unused variables
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
 }
